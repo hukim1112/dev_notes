a9ac077781a6fe894db1e1b0f8dbc641ca1eef4e
hukim <hyounguk1112@gmail.com>
2018-11-06 03:19:05 +0900

    update

--- setting/base.sh
+++ setting/base.sh
@@ -1,9 +1,27 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-dev
-sudo apt-get install python-pip
+
+sudo ln -sfn /usr/bin/python3.6 /usr/bin/python
+# change the default python version into 3.6
+
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
 
 cat >>.bashrc<<EOF
 export LC_ALL=C
 EOF
+
+cat >>$HOME/.bashrc<<EOF
+export WORKON_HOME=~/.virtualenvs
+. /usr/local/bin/virtualenvwrapper.sh
+EOF
+
 source ~/.bashrc
+
+mkvirtualenv envname --python=python3
+
+pip3 install ipython
+pip3 install --upgrade --force-reinstall --no-cache-dir jupyter
+pip3 install ipykernel
+
+python -m ipykernel install --user --name=envname
